361bd728e68e6ea59050ddb90e4da6cdfa511f70
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-04-06 18:57:44 -0700

    Through 7.3

---
+++ Weather Gift/Weather Location.swift
@@ -0,0 +1,15 @@
+//
+//  Weather Location.swift
+//  Weather Gift
+//
+//  Created by Claire Manley on 4/6/20.
+//  Copyright © 2020 Claire Manley. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
